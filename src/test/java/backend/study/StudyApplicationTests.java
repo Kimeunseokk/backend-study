a3e3ae03addf5d4d2ed4455856ea4fea6ad5d3eb
kimeunseokk <rpqhfls2whdk@gmail.com>
2025-09-14 18:02:48 +0900

    2025.09.14

--- src/test/java/backend/study/StudyApplicationTests.java
+++ src/test/java/backend/study/StudyApplicationTests.java
@@ -1,13 +1,29 @@
 package backend.study;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import backend.study.Entity.Menu;
+import backend.study.Repository.MenuRepository;
 
 @SpringBootTest
 class StudyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MenuRepository menuRepository;
 
+    @Test
+    void TestMenuSave() {
+        // 메뉴 객체 생성
+        Menu menu1 = new Menu();
+        menu1.setName("아메리카노");
+        menu1.setPrice(3500);
+        menu1.setSoldOut(false);
+
+        // DB에 저장 (H2 파일 DB 자동 생성)
+        Menu saved = menuRepository.save(menu1);
+
+        System.out.println("저장된 메뉴 ID: " + saved.getId());
+    }
 }
